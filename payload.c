eed2326f22f9a2447440e0bf2601139d0e4637eb
liamheidt <lhei0187@uni.sydney.edu.au>
2016-09-24 12:10:11 +1000

    Update payload.c

--- payload.c
+++ payload.c
@@ -3,14 +3,16 @@
 
 //Get raw data from the sensor, store with time and attitude in SD card
 void getRaw(int Attitude[], int Time) {
-  
+  // The code we already have to do 
   
 }
 //Gets raw data from SD card memory, process this and return in a form of your choosing to be transmitted. 
 int* process() {
+  // Post processing it, i.e the least squares
 }
 
 //Returns the amount of memory stored in SD card
 int memAvail() {
+  // FUCK ALL
   return 8;
 }
